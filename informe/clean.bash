dc3ec74f29d248f119aca183a6a2614e1632076c
Matías Pavez <matias.pavez.b@gmail.com>
2018-05-13 17:17:40 -0400

    portada y clean.bash

---
+++ informe/clean.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Run me to clean all tex products but the PDF file
+#
+# $ cd <this/dir>
+# $ ./clean.bash
+#
+#
+
+rm -f *.log
+rm -f *.aux
+rm -f *.out
+rm -f *.toc
+rm -f *.lof
+rm -f *.lot
+rm -f *.gz
+rm -f *.bbl
+rm -f *.blg
