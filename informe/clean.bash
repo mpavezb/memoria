09ea2a75a19588391152f6401710b8d6d56380ef
Matías Pavez <matias.pavez.b@gmail.com>
2018-05-26 05:38:55 -0400

    cleaning files

--- informe/clean.bash
+++ informe/clean.bash
@@ -7,12 +7,25 @@
 #
 #
 
+
 rm -f *.log
 rm -f *.aux
 rm -f *.out
 rm -f *.toc
+rm -f *.tdo
 rm -f *.lof
 rm -f *.lot
 rm -f *.gz
 rm -f *.bbl
 rm -f *.blg
+
+rm -f chapters/*.log
+rm -f chapters/*.aux
+rm -f chapters/*.out
+rm -f chapters/*.toc
+rm -f chapters/*.tdo
+rm -f chapters/*.lof
+rm -f chapters/*.lot
+rm -f chapters/*.gz
+rm -f chapters/*.bbl
+rm -f chapters/*.blg
